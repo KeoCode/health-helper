286ba2dcda513747edca222ab43bf90004193ca2
KeoCode <keocode101@gmail.com>
2022-09-12 21:38:02 +0000

    Add Javascript structure and prepared variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,42 @@
-console.log("hello from javascipt");
+let gender = document.getElementById('gender').value;
+let weight = document.getElementById('weight').value;
+let height = document.getElementById('height').value;
+let age = document.getElementById('age').value;
+let activityLevel = document.getElementById('activity-level').value;
+let goal = document.getElementById('goal').value;
+
+
+function calBMR() {
+
+}
+
+function calTEF() {
+
+}
+
+function calNEAT() {
+
+}
+
+function calTEA() {
+
+}
+
+function calMacros() {
+
+}
+
+function calLoss() {
+
+}
+
+function calGain() {
+
+}
+
+function calMaintain() {
+
+}
+
+console.log(resultsCAl);
+console.log(resultsMacro);
