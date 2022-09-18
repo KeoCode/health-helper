0ed8819156f891e73adc54a1d0829faa0023d6ef
KeoCode <keocode101@gmail.com>
2022-09-18 18:37:15 +0000

    Added event listener and fixed DOM get values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,63 @@
-let gender = document.getElementById('gender').value;
-let weight = document.getElementById('weight').value;
-let height = document.getElementById('height').value;
-let age = document.getElementById('age').value;
-let activityLevel = document.getElementById('activity-level').value;
-let goal = document.getElementById('goal').value;
+var form = document.getElementById('form');
+    
+ //Form Event Listener
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-let bmr, tdee;
+ //Get form values
+ 
+    var gender = document.querySelector('input[name="gender"]:checked').value;
+var weight = parseFloat(document.getElementById('weight').value);
+var height = parseFloat(document.getElementById('height').value);
+var age = parseInt(document.getElementById('age').value);
+var activityLevel = document.querySelector('input[name="activity-level"]:checked').value;
+var goal = document.querySelector('input[name="goal"]:checked').value;
 
-addEventListener
+   console.log(gender);
+   console.log(weight);
+   console.log(height);
+   console.log(age);
+   console.log(activityLevel);
+   console.log(goal);
 
-function calBmr() {
-    if (gender === female) {
+   calTdee();
+   
+   function calTdee() {
+    
+        let bmr = 0;
+        let tdee = 0;
+
+    if (gender === 'female') {
         bmr = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age);
       } else {
          bmr = 66 + (13.7 * weight) + (5 * height) - (6.8 * age);
       }
       console.log(bmr);
-    }
     
-    function calTdee() {
+
+
+    //Get TDEE (Total Daily Energy Expenditure), BMr times activity level
+    if (activityLevel == "sedentary") {
+        tdee = bmr * 1.2;
+    } else if (activityLevel === "lightly-active") {
+        tdee = bmr * 1.375;
+    } else if (activityLevel === "moderately-active") {
         tdee = bmr * 1.55;
+    } else if (activityLevel === "very-active") {
+        tdee = bmr * 1.725;
+    } else (activityLevel === "extemely-active"); {
+        tdee = bmr * 1.9;
     }
+    console.log(parseInt(tdee));
+}
+  
+    
     
    function calLossOne() {
-    let loseOne = tdee - 500;
-    let carbsLoseOne   = (loseOne * 0.3) / 4;
-    let fatLoseOne = (loseOne * 0.3) / 9;
-    let protienLoseOne = (loseOne * 0.4) /4;
+    var loseOne = tdee - 500;
+    var carbsLoseOne   = (loseOne * 0.3) / 4;
+    var fatLoseOne = (loseOne * 0.3) / 9;
+    var protienLoseOne = (loseOne * 0.4) /4;
     console.log(loseOne);
     console.log(carbsLoseOne);
     console.log(fatLoseOne);
@@ -48,18 +79,5 @@ function maintain() {
 
     console.log(tdee);
 }
-
-function calTEF() {
-
-}
-
-function calNEAT() {
-
-}
-
-function calTEA() {
-
-}
-
-console.log(resultsCAl);
-console.log(resultsMacro);
+  });
+    
