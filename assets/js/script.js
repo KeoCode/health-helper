bfb3a1c056af167954c3dc89a4c5ae1eaf337368
KeoCode <keocode101@gmail.com>
2022-09-21 18:34:09 +0000

    Changed an if loop to object and added main tag

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,80 @@
-let form = document.getElementById('form');
+var form = document.getElementById('form');
+
+function calTdee() {
+
+    var bmr = 0;
+    let tdee = 0;
+    var calories = 0;
+
+
+    //Calculate Basal metabolic Rate (BMR) by determining if it is female or male and calculating accordingly.
+    if (gender === 'female') {
+        bmr = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age);
+    } else {
+        bmr = 66 + (13.7 * weight) + (5 * height) - (6.8 * age);
+    }
+    console.log(bmr);
+
+
+
+    //Get TDEE (Total Daily Energy Expenditure), BMr times activity level
+    // if (activityLevel === "sedentary") {
+    //     tdee = bmr * 1.2;
+    // } else if (activityLevel === "lightly-active") {
+    //     tdee = bmr * 1.375;
+    // } else if (activityLevel === "moderately-active") {
+    //     tdee = bmr * 1.55;
+    // } else if (activityLevel === "very-active") {
+    //     tdee = bmr * 1.725;
+    // } else {
+    //     tdee = bmr * 1.9;
+    // }
+
+    let tdeeMap = {
+        sedentary: bmr * 1.2,
+        "lightly-active": bmr * 1.375,
+        "moderately-active": bmr * 1.55,
+        "very-active": bmr * 1.725,
+        "extremely-activity": bmr * 1.9,
+    };
+
+    tdee = tdeeMap[activityLevel];
+
+
+    console.log(parseInt(tdee));
+
+    // work out calories for each goal
+    if (goal === "loseOne") {
+        calories = tdee - 500;
+    } else if (goal === "loseTwo") {
+        calories = tdee - 1000;
+    } else if (goal === "gain") {
+        calories = tdee + 300;
+    } else if (goal === "maintain") {
+        calories = tdee;
+    }
+    console.log(parseInt(calories));
+
+
+    // Workout calories and grams for macros
+    var carbsCal = calories * 0.3;
+    var carbGram = carbsCal / 4;
+
+    var fatCal = calories * 0.3;
+    var fatGram = carbsCal / 9;
+
+    var proteinCal = calories * 0.4;
+    var proteinGram = carbsCal / 4;
+
+    //Log results to console to ensure is correctly calculated
+    console.log(parseInt(calories));
+    console.log(parseInt(carbsCal));
+    console.log(parseInt(carbGram));
+    console.log(parseInt(fatCal));
+    console.log(parseInt(fatGram));
+    console.log(parseInt(proteinCal));
+    console.log(parseInt(proteinGram));
+}
 
 //Form Event Listener
 form.addEventListener('submit', function (event) {
@@ -23,63 +99,6 @@ form.addEventListener('submit', function (event) {
 
     calTdee();
 
-    function calTdee() {
-
-        let bmr = 0;
-        let tdee = 0;
-        let calories = 0;
-
-        if (gender === 'female') {
-            bmr = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age);
-        } else {
-            bmr = 66 + (13.7 * weight) + (5 * height) - (6.8 * age);
-        }
-        console.log(bmr);
 
 
-
-        //Get TDEE (Total Daily Energy Expenditure), BMr times activity level
-        if (activityLevel === "sedentary") {
-            tdee = bmr * 1.2;
-        } else if (activityLevel === "lightly-active") {
-            tdee = bmr * 1.375;
-        } else if (activityLevel === "moderately-active") {
-            tdee = bmr * 1.55;
-        } else if (activityLevel === "very-active") {
-            tdee = bmr * 1.725;
-        } else {
-            tdee = bmr * 1.9;
-        }
-        console.log(parseInt(tdee));
-
-        // work out calories for each goal aswell as calories and gram for macros
-        if (goal === "loseOne") {
-            calories = tdee - 500;
-        } else if (goal === "loseTwo") {
-            calories = tdee - 1000;
-        } else if (goal === "gain") {
-            calories = tdee + 300;
-        } else if (goal === "maintain") {
-            calories = tdee;
-        }
-        console.log(parseInt(calories));
-
-        let carbsCal = calories * 0.3;
-        let carbGram = carbsCal / 4;
-
-        let fatCal = calories * 0.3;
-        let fatGram = carbsCal / 9;
-
-        let proteinCal = calories * 0.4;
-        let proteinGram = carbsCal / 4;
-//Log results
-        console.log(parseInt(calories));
-        console.log(parseInt(carbsCal));
-        console.log(parseInt(carbGram));
-        console.log(parseInt(fatCal));
-        console.log(parseInt(fatGram));
-        console.log(parseInt(proteinCal));
-        console.log(parseInt(proteinGram));
-    }
-    
 });
